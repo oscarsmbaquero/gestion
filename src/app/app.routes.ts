8c1520fad6af54232a191b3f5a8599b242aa23fe
oscar <oscarsmbaquero@gmail.com>
2025-03-24 12:17:16 +0100

    inicio

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,21 +2,27 @@ import { Routes } from '@angular/router';
 import { FacturasComponent } from '../pages/facturas/facturas.component';
 import { ChatboxComponent } from '../pages/chatbox/chatbox.component';
 import { ChatComponent } from '../shared/components/chat/chat.component';
+import { InicioComponent } from '../pages/inicio/inicio.component';
 
 export const routes: Routes = [
-    {
-        path: "facturas",//raiz de la app
-        pathMatch:'full',//coincida nombre exacto
-        component: FacturasComponent
-      },
-      {
-        path: 'chatbox',
-        pathMatch:'full',//coincida nombre exacto,
-        component: ChatboxComponent
-      },
-      { path: 'chat/:tipo', component: ChatComponent },
-      { 
-        path: '**', 
-        redirectTo: '' 
-      }
+  {
+    path: "facturas",//raiz de la app
+    pathMatch: 'full',//coincida nombre exacto
+    component: FacturasComponent
+  },
+  {
+    path: "",//raiz de la app
+    pathMatch: 'full',//coincida nombre exacto
+    component: InicioComponent
+  },
+  {
+    path: 'chatbox',
+    pathMatch: 'full',//coincida nombre exacto,
+    component: ChatboxComponent
+  },
+  { path: 'chat/:tipo', component: ChatComponent },
+  {
+    path: '**',
+    redirectTo: ''
+  }
 ];
