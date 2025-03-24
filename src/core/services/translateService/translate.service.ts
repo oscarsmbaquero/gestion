bc8c7ebe95b11ab1f23a29b1fa34206ffff374b3
oscar <oscarsmbaquero@gmail.com>
2025-03-24 10:29:42 +0100

    cards asistentes

--- src/core/services/translateService/translate.service.ts
+++ src/core/services/translateService/translate.service.ts
@@ -1,30 +1,3 @@
-// import { Injectable } from '@angular/core';
-// import { TranslateService } from '@ngx-translate/core';
-
-// @Injectable({
-//   providedIn: 'root'
-// })
-// export class TranslationService {
-
-//   defaultLang = 'es';
-
-//   constructor(private translateService: TranslateService) {
-//     this.translateService.setDefaultLang(this.defaultLang);
-    
-//     const browserLang = this.translateService.getBrowserLang();
-//     if (browserLang) {
-//       this.translateService.use(browserLang);
-//     }
-//   }
-
-//   changeLanguage(language: string): void {    
-//     this.translateService.use(language);
-//   }
-
-//   getCurrentLanguage(): string {
-//     return this.translateService.currentLang || this.defaultLang;
-//   }
-// }
 import { Injectable } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 import { BehaviorSubject, Observable } from 'rxjs';
