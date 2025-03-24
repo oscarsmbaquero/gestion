1c239c12910f7494b5137ee0088791a488fe43dc
oscar <oscarsmbaquero@gmail.com>
2025-03-24 11:01:59 +0100

    chat ok

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { FacturasComponent } from '../pages/facturas/facturas.component';
 import { ChatboxComponent } from '../pages/chatbox/chatbox.component';
+import { ChatComponent } from '../shared/components/chat/chat.component';
 
 export const routes: Routes = [
     {
@@ -13,6 +14,7 @@ export const routes: Routes = [
         pathMatch:'full',//coincida nombre exacto,
         component: ChatboxComponent
       },
+      { path: 'chat/:tipo', component: ChatComponent },
       { 
         path: '**', 
         redirectTo: '' 
